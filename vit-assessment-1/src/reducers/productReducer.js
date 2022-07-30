bf7f0998fe246f3e74eee570bc2f45f4fa63ef85
SUYASH <64803486+Suyash1507@users.noreply.github.com>
2022-07-30 13:05:34 +0530

    Add files via upload

---
+++ vit-assessment-1/src/reducers/productReducer.js
@@ -0,0 +1,60 @@
+export const getAllProductsReducer=(state={products : []} ,action)=>{
+
+    switch(action.type)
+    {
+        case 'GET_PRODUCTS_REQUEST' : return {
+            loading : true
+        }
+        case 'GET_PRODUCTS_SUCCESS' : return{
+            products : action.payload,
+            loading : false
+        }
+        case 'GET_PRODUCTS_FAILED' : return{
+            error : action.payload,
+            loading : false
+        }
+        default : return state
+    }
+}
+
+export const getProductByIdReducer=(state={product : {}} ,action)=>{
+
+
+    switch(action.type)
+    {
+        case 'GET_PRODUCTBYID_REQUEST' : return {
+            loading : true
+        }
+        case 'GET_PRODUCTBYID_SUCCESS' : return{
+            product : action.payload,
+            loading : false
+        }
+        case 'GET_PRODUCTBYID_FAILED' : return{
+            error : action.payload,
+            loading : false
+        }
+        default : return state
+    }
+
+}
+export const addProductReviewReducer=(state={},action)=>{
+
+
+    switch(action.type)
+    {
+        case 'ADD_PRODUCT_REVIEW_REQUEST' : return {
+            loading : true
+        }
+        case 'ADD_PRODUCT_REVIEW_SUCCESS' : return {
+           loading : false,
+           success : true
+       }
+       case 'ADD_PRODUCT_REVIEW_REQUEST' : return {
+           loading : false,
+           error:true
+       }
+       default : return state
+    }
+
+
+}
